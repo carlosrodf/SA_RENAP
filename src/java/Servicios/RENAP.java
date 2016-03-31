b6785fa5226f2002e89a3b7141ad80881bbc668b
Carlos Rodriguez <carlosrod.furlan@gmail.com>
2016-03-30 19:21:17 -0600

    Commit inicial

---
+++ src/java/Servicios/RENAP.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servicios;
+
+import db.dbConn;
+import java.sql.SQLException;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author carlosrodf
+ */
+@WebService(serviceName = "RENAP")
+public class RENAP {
+
+    /**
+     * Inscripcion de un ciudadano al registro
+     *
+     * @param dpi
+     * @param nombre1
+     * @param nombre2
+     * @param apellido1
+     * @param apellido2
+     * @param fechanac
+     * @param genero
+     * @param pais
+     * @param dpto
+     * @param municipio
+     * @return
+     */
+    @WebMethod(operationName = "Inscribirciudadano")
+    public boolean inscribir(@WebParam(name = "primer_nombre") String nombre1,
+            @WebParam(name = "segundo_nombre") String nombre2,
+            @WebParam(name = "primer_apellido") String apellido1,
+            @WebParam(name = "segundo_apellido") String apellido2,
+            @WebParam(name = "fecha_nacimiento") String fechanac,
+            @WebParam(name = "genero") String genero,
+            @WebParam(name = "pais") String pais,
+            @WebParam(name = "departamento") String dpto,
+            @WebParam(name = "municipio") String municipio) {
+
+        dbConn inf = new dbConn();
+        try {
+            int res =  inf.inscribirCiudadano(nombre1, nombre2, apellido1, apellido2, fechanac, genero, pais, dpto, municipio);
+            if(res > 0){
+                return true;
+            }else{
+                return false;
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            return false;
+        }
+    }
+}
---
+++ src/java/db/dbConn.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author carlosrodf
+ */
+public class dbConn {
+
+    private final String url;
+    private final String username;
+    private final String password;
+
+    public dbConn() {
+        this.url = "jdbc:mysql://ec2-54-186-237-148.us-west-2.compute.amazonaws.com:3306/mydb";
+        this.username = "*****";
+        this.password = "*****";
+    }
+
+    private Connection getConexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+
+        try {
+            System.out.println("1");
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("2");
+            return conn;
+        } catch (Exception e) {
+            System.out.println("No se pudo crear la conexion a la DB\n" + e);
+            return null;
+        }
+    }
+
+    public int inscribirCiudadano(String n1, String n2, String a1, String a2, String fecha, String gen, String pais, String dpto, String mun) throws SQLException {
+        Connection conn = getConexion();
+        if (conn != null) {
+            PreparedStatement query = conn.prepareStatement("INSERT INTO ciudadano"
+                    + "(primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,fecha_nac,genero,pais,departamento,municipio,estado) "
+                    + "VALUES (?,?,?,?,?,?,?,?,?,?,?);");
+
+            query.setString(1, n1);
+            query.setString(2, n2);
+            query.setString(3, a1);
+            query.setString(4, a2);
+            query.setString(5, fecha);
+            query.setString(6, gen);
+            query.setString(7, pais);
+            query.setString(8, dpto);
+            query.setString(9, mun);
+            query.setInt(10, 2);
+            return query.executeUpdate();
+        } else {
+            return -1;
+        }
+    }
+    
+    private int generarInscripcion(){
+        String correlativo;
+        String verificador;
+        
+        try {
+            ejecutar("INSERT INTO correlativos VALUES(null);");
+            ResultSet rs = ejecutarQuery("SELECT md5(inc) FROM correlativos ORDER BY inc DESC LIMIT 1;");
+            correlativo = rs.getString("inc");
+            
+            ejecutar("INSERT INTO verificadores VALUES(null);");
+            rs = ejecutarQuery("SELECT md5(inc) FROM verificadores ORDER BY inc DESC LIMIT 1;");
+            verificador = rs.getString("inc");
+            
+            ejecutar("INSERT INTO certificados('?','?',1,1,'?')");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(dbConn.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+    
+    //SIN RESULTADO
+    public void ejecutar(String sentencia) throws SQLException{
+        Connection conn = getConexion();
+        conn.prepareStatement(sentencia).execute();
+    }
+
+    //CON RESULTADO
+    public ResultSet ejecutarQuery(String query) throws SQLException {
+        Connection conn = getConexion();
+        Statement stmt = conn.createStatement();
+        return stmt.executeQuery(query);
+    }
+}
